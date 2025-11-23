cf55837977508e05b9dc00f44573dfd3aa9e773e
RosalieYelle <78310125+RosalieYelle@users.noreply.github.com>
2025-11-22 21:57:29 -0500

    Addition of the task model

---
+++ internal/models/task.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Task struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	UserID      string    `json:"userId"`
+}
